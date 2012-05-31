ccae09e180b0c2ff14045973bb38416de885ea3f
Jim Barrows <Jim.Barrows@gmail.com[color]>
2012-05-30 22:15:44 -0700

    Renamed create to form, and pointed the create view at it

--- city/views.py
+++ city/views.py
@@ -16,32 +16,5 @@ def view( request, city_id):
 	return render_to_response('city/view.html', {'city':c}, context_instance=RequestContext(request))
 
 def create( request):
-	return render_to_response('city/create.html', {'city_form':CityForm(),},context_instance=RequestContext(request))
+	return render_to_response('city/form.html', {'city_form':CityForm(),},context_instance=RequestContext(request))
 
-#class CreateCityWizardView(SessionWizardView):
-#	def done( self, form_list, **kwargs):
-#		city = City()
-#		for form in form_list:
-#			if form.__class__.__name__ == 'CityForm':
-#				city.name = form.cleaned_data['name']
-#				city.campaign_title = form.cleaned_data['campaign_title']
-#				city.save()
-#			elif form.__class__.__name__ == 'ConceptForm':
-#				concept = Concept()
-#				concept.city = city
-#				concept.theme = form.cleaned_data['theme']
-#				concept.idea = form.cleaned_data['idea']
-#				concept.save()
-#				aspect = Aspect()
-#				aspect.name = form.cleaned_data['aspect']
-#				aspect.concept = concept
-#				concept.aspect_set.add( aspect)
-#				city.concept_set.add( concept)
-#				concept.save()
-#		city.save()
-#		return  HttpResponseRedirect('/city/{0}'.format(city.id))
-
-#	def get_context_data(self, form, **kwargs):
-#		context = super(CreateCityWizardView, self).get_context_data(form=form, **kwargs)
-#		context.update({'wizard_name':'Create City Wizard',})
-#		return context
