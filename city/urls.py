50fc126f948f90dd005e3a3e68ceed71d71312d2
Jim Barrows <Jim.Barrows@gmail.com[color]>
2012-05-30 22:59:06 -0700

    can save the basic form.  Fixed an issue with view, it was only showing the mundane status quo, and not both

--- city/urls.py
+++ city/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
-from views import index, view, create
+from views import index, view, create, save
 from forms import CityForm, ConceptForm
 import os.path
 
@@ -10,5 +10,6 @@ APP_ROOT = os.path.join( os.path.dirname( __file__), '/web/city')
 urlpatterns = patterns('city.views',
 	url(r'^$', index),
 	url(r'^(?P<city_id>\d+)/$', view),
-	url(r'^create/$', create), #CreateCityWizardView.as_view([CityForm,ConceptForm,ConceptForm,ConceptForm])),
+	url(r'^create/$', create), 
+	url(r'^save/$', save), 
 )
--- city/views.py
+++ city/views.py
@@ -8,7 +8,7 @@ from city.models import City, Concept, Aspect
 from city.forms import CityForm
 
 def index( request):
-	city_list = City.objects.all().order_by('-name')[:5]
+	city_list = City.objects.all().order_by('-name')
 	return render_to_response( 'city/index.html', {'city_list' : city_list}, context_instance=RequestContext(request))
 
 def view( request, city_id):
@@ -17,4 +17,18 @@ def view( request, city_id):
 
 def create( request):
 	return render_to_response('city/form.html', {'city_form':CityForm(),},context_instance=RequestContext(request))
+
+def save( request):
+	if request.method == 'POST' :
+		city_form = CityForm(request.POST)
+		if city_form.is_valid():
+			city = City()
+			city.name = city_form.cleaned_data['name']
+			city.campaign_title = city_form.cleaned_data['campaign']
+			city.supernatural_status_quo = city_form.cleaned_data['supernatural_status_quo']
+			city.mundane_status_quo = city_form.cleaned_data['mundane_status_quo']
+			city.save()
+			return HttpResponseRedirect('/city/{0}'.format(city.pk))
+		else:
+			return render_to_response('city/form.html', {'city_form':city_form,},context_instance=RequestContext(request))
 
